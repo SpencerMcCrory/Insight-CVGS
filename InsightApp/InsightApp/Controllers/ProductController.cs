d3c50dd3f83072909b5f83aa54c1772a20703bbd
Eman Abdeen <eabdeen3804@conestogac.on.ca>
2024-10-20 01:08:22 -0400

    (Portal-Games, Portal-Game Details, Cart, Checkout)  UI

---
+++ InsightApp/InsightApp/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using InsightApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private InsightUpdateCvgs2Context _SVGSDbContext;
+        public ProductController(InsightUpdateCvgs2Context sVGSDbContext)
+        {
+            _SVGSDbContext = sVGSDbContext;
+        }
+
+        [HttpGet("/product-details")]
+        public IActionResult details()
+        {
+            return View();
+        }
+        [HttpGet("/products")]
+        public IActionResult products()
+        {
+            return View();
+        }
+
+        [HttpGet("/cart")]
+        public IActionResult cart()
+        {
+            return View();
+        }
+
+        [HttpGet("/cart/checkout")]
+        public IActionResult checkout()
+        {
+            return View();
+        }
+    }
+}
